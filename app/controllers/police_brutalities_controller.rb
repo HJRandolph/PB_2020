184fd7967aa839cde3cd48631609dbfff227c61b
HJRandolph <hjrandolph@gmail.com>
2020-06-24 13:35:37 -0400

    Updating to pull one random event from the API to display

--- app/controllers/police_brutalities_controller.rb
+++ app/controllers/police_brutalities_controller.rb
@@ -4,13 +4,18 @@ class PoliceBrutalitiesController < ApplicationController
 
   before_action :set_police_brutality, only: [:show, :edit, :update, :destroy]
 
+
   # GET /police_brutalities
   # GET /police_brutalities.json
   def index
 #	PoliceBrutality.paginate page: params[:page], per_page: 30
 	@police_brutalities = HTTParty.get("https://api.846policebrutality.com/api/incidents",
     :headers =>{'Content-Type' => 'application/json'} )
+
 	#@police_brutalities = @police_brutalities.paginate(page: params[:page], per_page: 30)
+	
+			i = @police_brutalities["data"].count
+		@random_b = @police_brutalities["data"][rand(0..i)]
   end
 
 
--- db/migrate/20200617201655_create_police_brutalities.rb
+++ db/migrate/20200617201655_create_police_brutalities.rb
@@ -1,7 +1,11 @@
 class CreatePoliceBrutalities < ActiveRecord::Migration[5.2]
   def change
     create_table :police_brutalities do |t|
-
+	  t.int :pb_id
+	  t.string :api_id
+	  t.string :state
+	  t.string :city
+	  t.string :title
       t.timestamps
     end
   end
