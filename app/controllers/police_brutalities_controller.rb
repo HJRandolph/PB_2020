f0b72c37e551c0736534cd04c2df43484e8d9895
HJRandolph <hjrandolph@gmail.com>
2020-06-26 18:51:35 -0400

    Pulling in more section from API. DRYing code

--- app/controllers/police_brutalities_controller.rb
+++ app/controllers/police_brutalities_controller.rb
@@ -3,19 +3,19 @@
 class PoliceBrutalitiesController < ApplicationController
 
  # before_action :set_police_brutality, only: [:show, :edit, :update, :destroy]
-
+	$police_brutalities = HTTParty.get("https://api.846policebrutality.com/api/incidents", :headers =>{'Content-Type' => 'application/json'} )
 
   # GET /police_brutalities
   # GET /police_brutalities.json
   def index
-	@police_brutalities = HTTParty.get("https://api.846policebrutality.com/api/incidents", :headers =>{'Content-Type' => 'application/json'} )
-	i = @police_brutalities["data"].count
-	@random_b = @police_brutalities["data"][rand(0..i)]
+	i = $police_brutalities["data"].count
+	@random_b = $police_brutalities["data"][rand(0..i)]
   end
 
   def show
-	@incidents = HTTParty.get("https://api.846policebrutality.com/api/incidents", :headers =>{'Content-Type' => 'application/json'} )
-	@police_brutalities = HTTParty.get("https://api.846policebrutality.com/api/incidents", :headers =>{'Content-Type' => 'application/json'} )
+
+
   end
 	
 end
+
