43678589e17f64fd734f9adc64c1d29f413fa7dd
HJRandolph <hjrandolph@gmail.com>
2020-06-26 18:23:01 -0400

    Adding additional page listing all incidents. Sticky nav for new page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :police_brutalities
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root 'police_brutalities#index'
+	
+	get 'show' => 'police_brutalities#show'
 end
